2bfc7ecaaeb312d0e0d063189d707de058f8345c
liju-liu <lijuliu1997@163.com>
2025-01-24 20:44:08 +0800

    Revert "Create Source_Attribution.m"
    This reverts commit ad80353e4c181b1c54146f67725545ebfe081897.

--- Source_Attribution.m
+++
@@ -1,4 +0,0 @@
-%The functions permEntropy.m and timeresolved_permEntropy.m used in this repository were originally 
-% shared on the Open Science Framework. We acknowledge and thank the authors for making their code publicly available.
-% The original code can be found here: https://osf.io/mr8f7/. We have included these functions in our analysis, 
-% and they are part of the methodology used to compute WPE and its time-resolved analysis.
